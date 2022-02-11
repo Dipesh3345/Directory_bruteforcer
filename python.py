9deb87e42744ef8ad307f56cf3a08dc52c7c5268
Dipesh <92223600+Dipesh3345@users.noreply.github.com>
2022-02-11 09:41:10 -0500

    Update python.py

--- python.py
+++ python.py
@@ -6,7 +6,7 @@ import argparse
 def getUserArguments():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-u", "--url", dest = "url", help = "[*] Please specipy a url or use --help ,for more info")
-	parse.add_argument("-f","--file", dest="file" , help ="[*] Please specify a word list , or use --help for more info")
+	parser.add_argument("-f","--file", dest="file" , help ="[*] Please specify a word list , or use --help for more info")
 	options = parser.parse_args()
 	return options
 
